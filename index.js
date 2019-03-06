4f4e1ea5da927fb9c0534be25dcc93b416e24e56
Csaba Palfi <csaba@palfi.me>
2019-03-06 09:45:24 +0000

    calibrate cpu

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
+const debug = require('debug');
 
-async function launchChromeAndRunLighthouse(url, opts, config = null) {
+async function runLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
     opts.port = chrome.port;
     return lighthouse(url, opts, config).then(results => {
@@ -10,30 +11,64 @@ async function launchChromeAndRunLighthouse(url, opts, config = null) {
   });
 }
 
+const metrics = [
+  'first-contentful-paint',
+  'first-meaningful-paint',
+  'speed-index',
+  'interactive',
+  'first-cpu-idle'
+];
+
 const opts = {
   chromeFlags: ['--headless']
 };
 
-const config = {
+const getConfig = (onlyAudits, cpuSlowdownMultiplier = 1) => ({
   extends: 'node_modules/lighthouse/lighthouse-core/config/lr-mobile-config.js',
   settings: {
-    onlyAudits: [
-      'first-contentful-paint',
-      'first-meaningful-paint',
-      'speed-index',
-      'interactive',
-      'first-cpu-idle'
-    ],
-    throttling: {
-      // TODO calibrate/adjust this based on benchmarkIndex
-      cpuSlowdownMultiplier: 3
-    }
+    onlyAudits,
+    throttling: {cpuSlowdownMultiplier}
   }
-};
+});
 
-(async() => {
-  const [,,url] = process.argv;
-  const result = await launchChromeAndRunLighthouse(url, opts, config);
-  const score = Math.round(result.categories.performance.score * 100);
-  console.log(score);
-})();
+function formatScore(score) {
+  return Math.round(score * 100);
+}
+
+async function calibrateCpu(url = 'http://www.example.com') {
+  const config = getConfig(metrics[0]);
+  const calibrationResult = await runLighthouse(url, opts, config);
+
+  const benchmarkIndex = calibrationResult.environment.benchmarkIndex;
+  const cpuSlowdownMultiplier = benchmarkIndex / 500;
+  debug('calibration')({ benchmarkIndex, cpuSlowdownMultiplier });
+
+  return cpuSlowdownMultiplier;
+}
+
+async function getPageSpeedScore(url, options = {}) {
+  const cpuSlowdownMultiplier =
+    options.cpuSlowdownMultiplier || await calibrateCpu();
+  const config = getConfig(metrics, cpuSlowdownMultiplier);
+  
+  const result = await runLighthouse(url, opts, config);
+
+  debug('result')(result);
+  Object.values(result.audits).forEach(({id, displayValue, score}) => 
+    debug('metrics')(`${id}: ${displayValue} (${formatScore(score)})`)
+  );
+
+  return formatScore(result.categories.performance.score);
+}
+
+if (require.main === module) {
+  (async() => {
+    const [,,url] = process.argv;
+    console.log(await getPageSpeedScore(url))
+  })();
+} else {
+  module.exports = {
+    calibrateCpu,
+    getPageSpeedScore
+  };
+}
