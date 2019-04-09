191b3a44cde0348e79330b4a0b429515ba0681a1
Csaba Palfi <csaba@palfi.me>
2019-04-09 17:46:22 +0100

    allow skipped runs (e.g. to warm CDN cache)

--- main.js
+++ main.js
@@ -8,17 +8,25 @@ export async function main() {
   const argv = yargs
     .boolean('local')
     .boolean('v')
+    .default('skippedRuns', 0)
     .default('runs', 9)
     .argv;
-  const {local, v: verbose, runs, _:[url]} = argv;
+  console.log(argv);
+  const {local, v: verbose, skippedRuns, runs, _:[url]} = argv;
   const test = local ? localTest : apiTest;
 
   const results = [];
   verbose && console.log('score\tTTFB\tFCP\tFMP\tSI\tFCI\tTTI\tbenchmarkIndex\tfetchTime')
-  for (let runsLeft = runs; runsLeft > 0; runsLeft--) {
+  for (let runsLeft = runs + skippedRuns; runsLeft > 0; runsLeft--) {
+    console.log(runsLeft, runs);
     const result = getMetrics(await test(url));
-    verbose && console.log(result.join('\t'));
-    results.push(result);
+    if (runsLeft < runs) {
+      verbose && console.log(result.join('\t'));
+      results.push(result);
+    } else {
+      const [score, ...rest] = result;
+      verbose && console.log([`(${score})`, ...rest].join('\t'));
+    }
   }
 
   console.log(median(results.map(([score, ...rest]) => score)))
