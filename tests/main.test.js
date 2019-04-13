44e8cea1380e14abd240cae17c21f1c98f104212
Csaba Palfi <csaba@palfi.me>
2019-04-14 00:55:40 +0100

    more tests

---
+++ tests/main.test.js
@@ -0,0 +1,24 @@
+const {format: tsvFormat} = require('../lib/tsv');
+const {format: jsonlFormat} = require('../lib/jsonl');
+const {main} = require('../lib/main');
+
+describe('main', () => {
+  const mockOptions = (overrides = {}) => ({
+    runs: 1, 
+    warmupRuns: 0, 
+    url: 'https://www.google.com', 
+    metrics: {},
+    jsonl: false,
+    ...overrides
+  });
+
+  it('uses tsvFormat by default', () => {
+    const runner = main(mockOptions());
+    expect(runner.format).toBe(tsvFormat);
+  });
+
+  it('uses jsonlFormat if specified', () => {
+    const runner = main(mockOptions({jsonl: true}));
+    expect(runner.format).toBe(jsonlFormat);
+  });
+});
