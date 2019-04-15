74c6667017df8178f4402e4b4397fb33a833d28e
Csaba Palfi <csaba@palfi.me>
2019-04-15 01:17:11 +0100

    fix user timing marks

--- tests/map-result.test.js
+++ tests/map-result.test.js
@@ -1,38 +1,25 @@
-const {mapUserTimingMarkValue, mapTTFB} = require('../lib/map-result');
+const {mapMarkValue, mapTTFB} = require('../lib/map-result');
 
 describe('map-result', () => {
-  describe('mapUserTimingMarkValue', () => {
-    const mark = 'my.timing.mark';
-    const mockAudits = (items) => ({
-      ['user-timings']: {
-        details: {items}
-      }
-    });
-
+  describe('mapMarkValue', () => {
     it('returns NaN if no mark found', () => {
-      const audits = mockAudits([]);
-      expect(mapUserTimingMarkValue(audits, mark))
+      expect(mapMarkValue(null))
         .toBe(NaN);
     });
 
     it('returns NaN if mark has no startTime', () => {
-      const audits = mockAudits([{name: mark}])
-      expect(mapUserTimingMarkValue(audits, mark))
+      expect(mapMarkValue({}))
         .toBe(NaN);
     });
 
     it('returns formatted ms value if mark has startTime', () => {
-      const audits = mockAudits([{name: mark, startTime: 785}])
-      expect(mapUserTimingMarkValue(audits, mark))
+      expect(mapMarkValue({startTime: 785}))
         .toBe(0.79);
     });
   });
 
   describe('mapTTFB', () => {
-    const name = 'time-to-first-byte';
-    const audits = {
-      [name]: {displayValue: 'Root document took 240 ms'}
-    };
-    expect(mapTTFB(audits, name)).toBe(0.24);
+    const audit = {displayValue: 'Root document took 240 ms'};
+    expect(mapTTFB(audit)).toBe(0.24);
   });
 });
