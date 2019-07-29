48ac797db9043d258072a87aa83ca8ca63b0c0e9
Kevin Olson <kolson@rentpath.come>
2019-07-29 17:24:25 -0500

    This add support for strategy option
    Adding the support of cli option `--strategy` to allow for desktop param to be
    passed to PSI v5. By default `--strategy` is set to `mobile`

--- tests/run-pagespeed.test.js
+++ tests/run-pagespeed.test.js
@@ -5,15 +5,28 @@ jest.mock('wreck');
 describe('run-pagespeed', () => {
   const lighthouseResult = {};
 
-  it('runPageSpeed calls API and returns Lighthouse result', async () => {
+  it('runPageSpeed calls Mobile API and returns Lighthouse result', async () => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1479427200000);
     wreck.get = jest.fn()
       .mockResolvedValue({payload: {lighthouseResult}});
-  
+
     const {result} = await runPagespeed('https://www.google.com');
 
     expect(result).toBe(lighthouseResult);
     expect(wreck.get).toHaveBeenCalledTimes(1)
     expect(wreck.get.mock.calls[0]).toMatchSnapshot();
   });
+
+  it('runPageSpeed calls Desktop API and returns Lighthouse result', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => 1479427200000);
+    wreck.get = jest.fn()
+      .mockResolvedValue({payload: {lighthouseResult}});
+
+    const {result} = await runPagespeed('https://www.google.com', 'desktop');
+
+    expect(result).toBe(lighthouseResult);
+    expect(wreck.get).toHaveBeenCalledTimes(1)
+    expect(wreck.get.mock.calls[0]).toMatchSnapshot();
+  });
+
 });
