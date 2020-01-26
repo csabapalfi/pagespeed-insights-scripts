cb703a5363da4b1fac4e2f0f47ebba47995750f3
Csaba Palfi <csaba@palfi.me>
2020-01-26 21:39:47 +0000

    remove support for warmup runs

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -5,27 +5,12 @@ describe('Counter', () => {
   it('returns a single first and last flag in the correct positions', () => {
     fc.assert(
       fc.property(
-        fc.integer(1, 100), fc.integer(0, 100), (r, w) => { 
-          const runs = [...new Counter(r, w)];
+        fc.integer(1, 100), (r) => { 
+          const runs = [...new Counter(r)];
           expect(runs.filter(({first}) => first)).toHaveLength(1);
           expect(runs[0]).toHaveProperty('first', true);
           expect(runs.filter(({last}) => last)).toHaveLength(1);
           expect(runs[runs.length -1]).toHaveProperty('last', true);
-        }
-      )
-    );
-  });
-
-  it('returns the configured number of warmup and normal runs', () => {
-    fc.assert(
-      fc.property(
-        fc.integer(1, 100), fc.integer(0, 100), (r, w) => { 
-          const runs = [...new Counter(r, w)];
-          expect(runs.map(({warmup}) => warmup))
-            .toEqual([
-              ...new Array(w).fill(true),
-              ...new Array(r).fill(false)
-            ])
         }
       )
     );
